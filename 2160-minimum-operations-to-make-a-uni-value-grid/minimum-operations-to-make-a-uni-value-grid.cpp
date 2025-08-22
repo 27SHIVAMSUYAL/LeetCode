a7d107107cec603a238ee62849966de333a0f876
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-08-23 03:57:36 +0530

    Time: 31 ms (81.80%) | Memory: 86.9 MB (86.46%) - LeetSync

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        int row = grid.size(), col = grid[0].size();
+
+        int count = 0 ;
+        vector<int>arr;
+        int remainder =  remainder =grid[0][0]%x;
+        for (int i = 0; i < row; i++) {
+
+            for (int j = 0; j < col; j++) {
+
+  
+                arr.push_back(grid[i][j]);
+                if(grid[i][j]%x != remainder){
+                    return -1;
+                }
+            }
+        }
+
+        sort(arr.begin(),arr.end());
+        int mid = arr.size()/2;
+
+        int median = arr[mid];
+
+        for(int i = 0 ; i < arr.size();i++){
+            count += abs(arr[i]-median)/x;
+        }
+       
+        return count;
+    }
+};
