7a35573057dc897eaf22f70b9c21c989a50e6f1a
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-08-23 03:58:04 +0530

    Time: 40 ms (57.21%) | Memory: 87.2 MB (77.87%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -19,9 +19,7 @@ public:
         }
 
         sort(arr.begin(),arr.end());
-        int mid = arr.size()/2;
-
-        int median = arr[mid];
+        int median = arr[arr.size()/2];
 
         for(int i = 0 ; i < arr.size();i++){
             count += abs(arr[i]-median)/x;
