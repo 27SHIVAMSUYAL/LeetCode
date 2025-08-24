fab3b692b4480abe1214a8f0884e49b0b45d501d
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-08-24 14:51:23 +0530

    Time: 1 ms (96.63%) | Memory: 65 MB (69.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,23 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-
-    let profit = 0  , min = Number.MAX_SAFE_INTEGER , max = Number.MIN_SAFE_INTEGER;
-
-    let mp = new Map();
-
-    for( let i = 0 ; i < prices.length ; i++){
-        if(prices[i] < min){
-            min=prices[i];
-            mp.set(prices[i] , i);
-            max = Number.MIN_SAFE_INTEGER;
-        }else if( prices[i] > max && mp.get(min)<i){
-                profit = Math.max(profit , prices[i]-min);
-                max = prices[i];
+    
+let profit = 0 , min = Number.MAX_SAFE_INTEGER;
+    for(let i = 0 ; i <prices.length ; i++){
+        if( prices[i] < min){
+            min = prices[i];
+        }else {
+            profit = Math.max(prices[i]-min , profit);
         }
     }
 
-
     return profit;
-    
 };
