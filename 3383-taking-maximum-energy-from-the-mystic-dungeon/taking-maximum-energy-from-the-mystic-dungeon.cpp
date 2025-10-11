3069baa7a5d5bad830add57e94292c5773584a8e
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-10-12 00:37:03 +0530

    Time: 132 ms (80.53%) | Memory: 151.6 MB (50.17%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -3,31 +3,22 @@ public:
     int maximumEnergy(vector<int>& energy, int k) {
 
         int maxEnergy = INT_MIN , l = energy.size();
-        map<int,int> dp;
+        vector<int> dp(l,0);
 
         for(int i = l-1 ; i >= 0 ;i--){
 
-            maxEnergy = max(maxEnergy , solve(energy , k , i ,dp));
+            if(i + k < l ){
+                dp[i] = dp[i+k] + energy[i];
+            }else{
+                dp[i] = energy[i];
+            }
+
+            maxEnergy = max(dp[i] , maxEnergy);
         }
 
         return maxEnergy;
         
     }
 
-    int solve(vector<int> &energy , int k , int i , map<int,int> &dp ){
-        int temp = i;
-        int sum = 0 ;
-        int l = energy.size();
-
-         if(i+k < l ){
-                sum = energy[i] + dp[i+k];
-                return dp[temp]= sum;
-            }
-
-        // while( i < l ){
-        //     sum += energy[i];
-        //     i = i+k ;
-        // }
-        return dp[temp] = energy[i];
-    }
+    
 };
