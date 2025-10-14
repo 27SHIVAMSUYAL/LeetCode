13d33114b7f4c2c2f04e9bbf9dd20d1009eb1444
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-10-14 13:35:12 +0530

    Time: 12 ms (27.87%) | Memory: 25.9 MB (88.97%) - LeetSync

--- 554-brick-wall/brick-wall.cpp
+++ 554-brick-wall/brick-wall.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
-
-        int totalWidth = wall.size();
-        int totalHeight = wall.size();
         uint sum;
         int maxi = 0;
         unordered_map<int, int> map;
@@ -11,7 +8,7 @@ public:
         for (int i = 0; i < wall.size(); i++) {
             sum = 0;
             for (int j = 0; j < wall[i].size()-1; j++) {
-                // sum = sum + (1LL * wall[i][j]);
+               
                 sum = sum + wall[i][j];
                 if (map.find(sum) != map.end()) {
                     map[sum]++; // already exists → increment
@@ -22,6 +19,6 @@ public:
             }
         }
 
-        return totalHeight - maxi;
+        return wall.size() - maxi;
     }
 };
