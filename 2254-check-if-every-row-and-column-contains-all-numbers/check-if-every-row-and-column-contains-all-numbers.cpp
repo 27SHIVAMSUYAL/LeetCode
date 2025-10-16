e39cce7105976118d3fa607da4fb7f2c16957260
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-10-16 14:01:50 +0530

    Time: 8 ms (71.53%) | Memory: 43.5 MB (59.08%) - LeetSync

---
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+
+
+        int n = matrix.size();
+
+        
+
+        for(int i = 0 ; i < n ;i++){
+          if(checkRow( i , matrix  , n) == false){
+            return false;
+          }
+        }
+
+        for(int j = 0 ; j < n ; j++){
+          if(checkCol( j , matrix  , n) == false){
+            return false;
+          }
+        }
+
+        return true;
+        
+    }
+
+    bool checkRow( int i , vector<vector<int>>& matrix , int n){
+
+        vector<int>count(n+1,0);
+
+                int sum = n*(n+1)/2;
+
+        for(int j = 0 ; j < n ; j++){
+            if(count[ matrix[i][j] ] == 0){
+                count[ matrix[i][j] ]++;
+                sum -= matrix[i][j];
+            }
+        }
+
+        if(sum == 0 ){return true;}
+
+        return false;
+
+
+    }
+
+    bool checkCol( int j , vector<vector<int>>& matrix , int n){
+
+        vector<int>count(n+1,0);
+
+          int sum = n*(n+1)/2;
+
+        for(int i = 0 ; i < n ; i++){
+            if(count[ matrix[i][j] ] == 0){
+                count[ matrix[i][j] ]++;
+                 sum -= matrix[i][j];
+
+            }
+        }
+
+          if(sum == 0 ){return true;}
+
+        return false;
+
+
+    }
+};
