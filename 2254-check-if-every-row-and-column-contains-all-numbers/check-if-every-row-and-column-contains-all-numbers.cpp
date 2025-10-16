933e76e14157b9cc948025bbd161432d9f0fb78f
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-10-16 14:10:26 +0530

    Time: 2 ms (91.29%) | Memory: 43.5 MB (59.08%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -5,16 +5,18 @@ public:
 
         int n = matrix.size();
 
-        
+        vector<int>count(n+1,0);
+
+        int sum = n*(n+1) / 2;
 
         for(int i = 0 ; i < n ;i++){
-          if(checkRow( i , matrix  , n) == false){
+          if(checkRow( i , matrix  , n , count , sum) == false){
             return false;
           }
         }
 
         for(int j = 0 ; j < n ; j++){
-          if(checkCol( j , matrix  , n) == false){
+          if(checkCol( j , matrix  , n , count , sum) == false){
             return false;
           }
         }
@@ -23,11 +25,9 @@ public:
         
     }
 
-    bool checkRow( int i , vector<vector<int>>& matrix , int n){
+    bool checkRow( int i , vector<vector<int>>& matrix , int n , vector<int> count ,int sum){
 
-        vector<int>count(n+1,0);
-
-                int sum = n*(n+1)/2;
+    
 
         for(int j = 0 ; j < n ; j++){
             if(count[ matrix[i][j] ] == 0){
@@ -43,11 +43,9 @@ public:
 
     }
 
-    bool checkCol( int j , vector<vector<int>>& matrix , int n){
+    bool checkCol( int j , vector<vector<int>>& matrix , int n , vector<int> count , int sum){
 
-        vector<int>count(n+1,0);
-
-          int sum = n*(n+1)/2;
+    
 
         for(int i = 0 ; i < n ; i++){
             if(count[ matrix[i][j] ] == 0){
