9be86686b0ff62e111a2045584bed86b49cb1b76
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2025-10-30 00:14:58 +0530

    Time: 3 ms (6.17%) | Memory: 9.4 MB (11.95%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -2,19 +2,33 @@ class Solution {
 public:
     int smallestNumber(int n) {
 
-        string bit = bitset<16>(n).to_string();
-        int flag = 0 ;
+       // create the binary
 
-        cout<<"bit " << bit << '\n';
-        for( char &ch : bit){
-            if(flag ==1){
-                ch ='1';
-            }else if(ch == '1'){
-                flag = 1;
-            }
-        }
+       int ans = 0 ;
+       int digit;
 
-        return stoi(bit , 0 , 2);
+       string binary = "";
+      
+       while( n > 0){
+        digit = n%2;
+        binary = to_string(digit) + binary;
+        n = n/2;
+       }
+         cout<<"binary " << binary << '\n';
+       int power = 0;
+       int num = stoi(binary);
+           cout<<"num" << num << '\n';
+
+       while( num > 0 ){
+        digit = num%10;
+
+            ans += pow(2 , power);
+
+        power++;
+        num = num/10;
+       }
+
+       return ans;
         
     }
 };
