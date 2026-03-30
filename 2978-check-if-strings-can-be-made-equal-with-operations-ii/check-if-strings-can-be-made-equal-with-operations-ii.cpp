17ba7efeeba20950ecba257afccebad4245d7bca
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2026-03-31 01:38:21 +0530

    Time: 69 ms (8.93%) | Memory: 20.8 MB (33.31%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,46 +2,58 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
 
-    vector<char> s1Even , s1Odd , s2Even , s2Odd;
+    map< char , int> even , odd;
 
 
     for( int i = 0 ; i < s1.size() ;i++ ){
         
         if(i%2 == 0){
-            s1Even.push_back(s1[i]);
+
+            if( even.find(s1[i])  == even.end()){
+                even[s1[i]] = 1;
+            }else{
+                even[s1[i]]++;
+            }
+            
         }else{
-            s1Odd.push_back(s1[i]);
+
+            if( odd.find(s1[i])  == odd.end()){
+                odd[s1[i]] = 1;
+            }else{
+                odd[s1[i]]++;
+            }
         }
         
     }
 
     for( int i = 0 ; i < s2.size() ; i++){
         if( i%2 == 0){
-            s2Even.push_back(s2[i]);
+            if( even.find(s2[i]) != even.end()){
+                even[s2[i]]--;
+            }
 
         }else{
-            s2Odd.push_back(s2[i]);
+            if( odd.find(s2[i]) != odd.end()){
+                odd[s2[i]]--;
+            }
+            
         }
     }
 
 
-    sort(s1Even.begin() , s1Even.end());
-    sort(s2Odd.begin() , s2Odd.end());
-    sort(s2Even.begin( ) , s2Even.end());
-    sort(s1Odd.begin() , s1Odd.end());
+    
 
-
-    for( int i = 0 ; i < s1Even.size() ;i++){
-        if(s1Even[i] != s2Even[i]){
+    for( pair<char , int > p : even){
+        if(p.second != 0){
             return false;
         }
     }
 
 
-    for( int i = 0 ; i < s2Odd.size() ; i++){
-        if( s2Odd[i] != s1Odd[i]){
-            return false;
-        }
+    for( pair<char , int > p : odd){
+      if(p.second != 0){
+        return false;
+      }
     }
 
     return true;
