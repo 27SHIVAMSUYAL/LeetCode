f14611e449393e6ff7f0440ad5dba15193e3d6df
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2026-05-10 01:34:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (72.47%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -2,41 +2,18 @@ class Solution {
 public:
     int countGoodSubstrings(string s) {
 
-        int n = s.size() , answer = 0;
+        int n = s.size();
+        int answer = 0;
 
-        for( int i = 0 ; i < n-2 ; i++){
-            
+        for( int i = 0 ; i < n - 2; i++){
 
-                check(s,i , i+2 , answer);
+                if( s[i] != s[i+1] && s[i] != s[i+2] && s[i+1] != s[i+2]){
+                    answer++;
+                }
 
-            
         }
 
         return answer;
         
     }
-
-    void check(string &s , int i , int j , int &answer){
-        unordered_set<char> sett;
-        cout<< "\n";
-        while(i <= j ){
-                 cout<< i;
-            auto it = sett.find(s[i]);
-
-            if(it == sett.end()){
-                sett.insert(s[i]);
-                
-            }else{
-                return;
-            }
-            i++;
-
-        }
-        answer++;
-        return;
-
-    }
-
-
-
 };
