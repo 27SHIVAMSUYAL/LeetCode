3f9dcd0925a76d057791328981227399631e1124
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2026-05-10 12:16:20 +0530

    Time: 43 ms (51.56%) | Memory: 26.5 MB (13.30%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -5,13 +5,15 @@ public:
       
 
         vector<string>answer;
-       
+          if(s.size() < 10){
+            return answer;
+        }
 
         unordered_map<string,int>mapp;
 
         string ss;
 
-        for( int i = 0 ; i <= ((s.size()-10)%s.size() + s.size())%s.size() ; i++){
+        for( int i = 0 ; i <= s.size()-10 ; i++){
             ss = s.substr(i, 10);
             auto it = mapp.find(ss);
 
