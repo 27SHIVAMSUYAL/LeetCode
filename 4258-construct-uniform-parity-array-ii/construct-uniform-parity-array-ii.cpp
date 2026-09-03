0b577a198845ac39ed62d1bcdde672878d4a5d76
SHIVAM SUYAL <131432506+27SHIVAMSUYAL@users.noreply.github.com>
2026-09-04 02:01:23 +0530

    Time: 19 ms (26.11%) | Memory: 181.2 MB (6.16%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         
-
-        int mini = INT_MAX;
-
-        if( odd(mini , nums1)){
+        if( check( nums1 , 1)){
             return true;
-        }else if(even(mini , nums1)){
+        }else if(check( nums1 , 0)){
             return true;
         }
         return false;
     }
 
-    void findMini(int &mini , vector<int>&nums , int flag){
+    int findMini(int mini , vector<int>&nums , int flag){
         for( int i = 0 ; i < nums.size() ; i++){
             if( flag == 0 && nums[i]%2 == 0){
                 mini = min(nums[i] , mini);
@@ -21,34 +18,20 @@ public:
                 mini = min(nums[i] , mini);
             }
         }
+        return mini;
     }
 
-    bool odd(int &mini , vector<int>&nums1 ){
-        mini = INT_MAX;
-        findMini(mini , nums1 , 1);
+    bool check( vector<int>&nums1 , int flag ){
+        int mini = findMini(INT_MAX , nums1 , 1);
         vector<int>nums2;
         for(int i = 0 ; i < nums1.size(); i++){
-            if(nums1[i]%2 != 0){
+            if(flag == 1 && nums1[i]%2 != 0){
                 nums2.push_back(nums1[i]);
-            }else if(nums1[i] - mini >= 1 && ( nums1[i] - mini )%2 != 0 ){
+            }else if(flag == 1 && nums1[i] - mini >= 1 && ( nums1[i] - mini )%2 != 0 ){
                 nums2.push_back(nums1[i]);
-            }else{
-                return false;
-            }
-        }
-        return true;
-        
-    }
-
-
-    bool even(int &mini , vector<int>&nums1 ){
-        mini = INT_MAX;
-        findMini(mini , nums1 , 0);
-        vector<int>nums2;
-        for(int i = 0 ; i < nums1.size(); i++){
-            if(nums1[i]%2 == 0){
+            }else if(flag == 0 && nums1[i]%2 == 0){
                 nums2.push_back(nums1[i]);
-            }else if(nums1[i] - mini >= 1 && ( nums1[i] - mini )%2 == 0 ){
+            }else if(flag == 0 && nums1[i] - mini >= 1 && ( nums1[i] - mini )%2 == 0 ){
                 nums2.push_back(nums1[i]);
             }else{
                 return false;
